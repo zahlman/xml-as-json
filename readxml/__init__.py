b5d8c9e2ea794d12bed1b55eca9fd8755ada734e
Karl Knechtel <karl.a.knechtel@gmail.com>
2022-01-17 16:21:18 -0500

    parse basic structure, interweaving child tags and non-empty text nodes

--- readxml/__init__.py
+++ readxml/__init__.py
@@ -1,1 +1,38 @@
+from xml.etree import ElementTree as ET
+
+
 __version__ = '0.1.0'
+
+
+def normalize(text):
+    if text is None:
+        return ''
+    return text.strip()
+
+
+def children(element):
+    lead = normalize(element.text)
+    if lead:
+        yield lead
+    for c in element:
+        yield c
+        trail = normalize(c.tail)
+        if trail:
+            yield trail
+
+
+def build(element):
+    if isinstance(element, str):
+        return element
+    result = dict(element.attrib)
+    result[''] = element.tag
+    result['[[]]'] = [build(c) for c in children(element)]
+    return result
+
+
+def parse(source):
+    if isinstance(source, str) and source.startswith('<'):
+        xml = ET.fromstring(source)
+    else:
+        xml = ET.parse(source).getroot()
+    return build(xml)
