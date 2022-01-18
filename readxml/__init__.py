959c30c555a9638231699f467ce0e95c094435c8
Karl Knechtel <karl.a.knechtel@gmail.com>
2022-01-17 23:19:39 -0500

    add method to get simple output from a restricted subset of xml input

--- readxml/__init__.py
+++ readxml/__init__.py
@@ -30,9 +30,43 @@ def build(element):
     return result
 
 
-def parse(source):
+def simple(element):
+    result = dict(element.attrib)
+    anames = set(result.keys())
+    # we don't care about the root tag name, and child tag names will
+    # be implicit in the dict we're making.
+    lead = normalize(element.text)
+    for c in element:
+        if c.tag in anames:
+            raise ValueError(c.tag, 'conflict between attributes and children')
+        if normalize(c.tail):
+            raise ValueError('mixed text data and child tags found')
+        to_add = simple(c)
+        lname = f'[{c.tag}]'
+        if lname in result:
+            result[lname].append(to_add)
+        elif c.tag in result:
+            result[lname] = [result[c.tag], to_add]
+            del result[c.tag]
+        else:
+            result[c.tag] = to_add
+    if lead and result:
+        raise ValueError('mixed text data and attributes/child tags found')
+    if result:
+        return result
+    if lead:
+        for cast in (int, float):
+            try:
+                return cast(lead)
+            except ValueError:
+                pass
+        return lead
+    return True # blank tag
+
+
+def parse(source, builder='build'):
     if isinstance(source, str) and source.startswith('<'):
         xml = ET.fromstring(source)
     else:
         xml = ET.parse(source).getroot()
-    return build(xml)
+    return {'build': build, 'simple': simple}[builder](xml)
